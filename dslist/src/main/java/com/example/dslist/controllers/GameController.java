d5408c2a9eff3e44a4aac161924767e79d9ed259
Rodrigo Matsumoto <matsumoto.rodrigo1@gmail.com>
2025-01-23 22:25:08 -0300

    get game by id

--- dslist/src/main/java/com/example/dslist/controllers/GameController.java
+++ dslist/src/main/java/com/example/dslist/controllers/GameController.java
@@ -1,9 +1,11 @@
 package com.example.dslist.controllers;
 
+import com.example.dslist.dtos.GameDTO;
 import com.example.dslist.dtos.GameMinDTO;
 import com.example.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +17,11 @@ public class GameController {
   
   @Autowired
   private GameService gameService;
+  
+  @GetMapping(value = "/{id}")
+  public GameDTO findById(@PathVariable Long id) {
+    return gameService.findById(id);
+  }
   
   @GetMapping
   public List<GameMinDTO> findAll() {
--- dslist/src/main/java/com/example/dslist/services/GameService.java
+++ dslist/src/main/java/com/example/dslist/services/GameService.java
@@ -1,10 +1,12 @@
 package com.example.dslist.services;
 
+import com.example.dslist.dtos.GameDTO;
 import com.example.dslist.dtos.GameMinDTO;
 import com.example.dslist.entities.Game;
 import com.example.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -14,6 +16,14 @@ public class GameService {
   @Autowired
   private GameRepository gameRepository;
   
+  @Transactional(readOnly = true)
+  public GameDTO findById(Long id) {
+    Game result = gameRepository.findById(id).orElseThrow();
+    
+    return new GameDTO(result);
+  }
+  
+  @Transactional(readOnly = true)
   public List<GameMinDTO> findAll() {
     List<Game> result = gameRepository.findAll();
     
